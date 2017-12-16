8a69b99a6ef7fcf6bb6a4290fbcd7db73a83e606
MarcoFalke <falke.marco@gmail.com>
2017-12-16 15:14:57 -0500

    Update to most recent csv format

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # BitcoinGraphs
+
+To keep your system from clutter, setting up a python virtual environment is recommended:
+
+```sh
+virtualenv --python=python3 ./env_3
+```
+
+Make sure to install the dependencies
+```sh
+source ./env_3/bin/activate
+pip install matplotlib pandas seaborn
+```
+
+To run
+```sh
+source ./env_3/bin/activate
+python3 ./plot_bench.py -h
+```
--- plot_bench.py
+++ plot_bench.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import matplotlib
+from collections import namedtuple
 import numpy as np
 import pandas as pd
 import seaborn as sn
@@ -10,41 +11,36 @@ import os
 import argparse
 import collections
 
+Bench = namedtuple('Bench', 'file, times')
+
 
 def get_benches(folder):
     print('Collecting benchmarks ...')
     benches = collections.defaultdict(list)
+    csv_dialect = csv.excel  # The default dialect
+    csv_dialect.skipinitialspace = True
     for file in sorted([f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]):
         with open(os.path.join(folder, file), newline='') as csvfile:
-            print(' ... found bench file {}'.format(file))
-            rows = [row for row in csv.reader(csvfile, delimiter=',')]
+            print(' ... found bench file "{}"'.format(file))
+            rows = [row for row in csv.reader(csvfile, dialect=csv_dialect, delimiter=',')]
             labels, rows = rows[0], rows[1:]
             for row in rows:
                 bench_name = row[0]
-                data_frame = pd.DataFrame(data=row[1:], index=labels[1:])
-                times = data_frame.loc[['min(ns)', 'max(ns)', 'average(ns)']]
-                cycles = data_frame.loc[['min_cycles', 'max_cycles', 'average_cycles']]
-                files = [[file]] * len(cycles)
-                data_frame = pd.DataFrame(data=np.concatenate([times, cycles, files], axis=1),
-                                          columns=['times', 'cycles', 'files'])
-                benches[bench_name] += [data_frame]
-    for bench_name in benches:
-        benches[bench_name] = pd.concat(benches[bench_name], axis=0)
+                # Make sure to break when the format changes
+                assert labels == ['# Benchmark', 'evals', 'iterations', 'total', 'min', 'max', 'median']
+                times = row[4:]
+                benches[bench_name] += [Bench(file, times)]
     return benches
 
 
 def plot_benches(benches, out_folder):
     print('Creating plots for benches ...')
     for bench_name in benches:
-        bench = benches[bench_name]
-        #target = 'cycles'
-        target = 'times'
-        sn.boxplot(
-            x=bench['files'],
-            y=bench[target].apply(pd.to_numeric),
-        )
-        pl.xlabel('Instance')
-        pl.ylabel(target)
+        files = [b.file for b in benches[bench_name]]
+        time_data = [pd.Series(b.times).apply(pd.to_numeric) for b in benches[bench_name]]
+        sn.boxplot(x=files, y=time_data)
+        #pl.xlabel('File')
+        pl.ylabel('time')
         pl.ylim(ymin=0)
         pl.title(bench_name)
         print(' ... {}'.format(bench_name))
